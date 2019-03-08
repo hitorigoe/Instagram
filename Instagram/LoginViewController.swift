c40b2171bd6adc7dfcab1e12b42d0847dd1fe754
鳥越洋之 <torigoe@torigoehiroyukinomacbook-pro.local>
2019-03-09 03:56:06 +0900

    gitignoreまで作成

---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by 鳥越洋之 on 2019/03/09.
+//  Copyright © 2019 鳥越洋之. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
